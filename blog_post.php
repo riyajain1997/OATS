d114770f99577b0355cce28a3c0d94629f757553
jackInf777 <jigneshmahadik777@gmail.com>
2020-09-19 14:18:38 +0530

    blog_post,blog,ownblog,blog_details

--- blog_post.php
+++ blog_post.php
@@ -1,10 +1,39 @@
+<?php
+    include_once("DbConnection.php");
+    $Bid=$_GET['bid'];
+
+    if(isset($_POST['btnsubmit']))
+    {
+        $uid=$_SESSION['UserID'];
+        $title=$_POST['txttitle'];
+        $content=$_POST['txtcontent'];
+        $date=date("y/m/d");
+
+        $image=$_FILES['txtimage']['name'];
+        $tmp_name=$_FILES['txtimage']['tmp_name'];
+        $path="Uploaded/Images/".$image;
+
+        move_uploaded_file($tmp_name,$path);
+        $query_blog="insert into tblblogs(Title,Content,File,CreatedDate,Uid,IsActive)values('$title','$content','$image',now(),$uid,1)";
+        $execute=mysqli_query($con,$query_blog);
+
+        if($execute)
+        {
+            echo '<script type="text/javascript">alert("Blog posted successfully...");</script>';
+        }
+        else
+        {
+            echo "error".mysqli_error($con);
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="zxx">
     <head>
         <?php include_once('cssLinks.php');?>
     </head>
     <body>
-
 
         <!-- Start Navbar Area -->
         <?php include_once('headerAlumni.php');?>
@@ -40,25 +69,25 @@
                 <div class="tab-content" id="pills-tabContent">
                     <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
                         <div class="create-information">
-                            <form method="POST">
+                            <form method="POST" enctype="multipart/form-data">
                                 <h3>Post a Blog</h3>
                                 <div class="row">
                                     <div class="col-lg-12">
                                         <div class="form-group">
                                             <label>Post Title:</label>
-                                            <input type="text" name="txttitle" class="form-control" placeholder="Title">
+                                            <input type="text" name="txttitle" id="txttitle" class="form-control" placeholder="Title">
                                         </div>
                                     </div>
                                     <div class="col-lg-12">
                                         <div class="form-group">
                                             <label>Post Content:</label>
-                                            <textarea name="txtcontent" rows="8" class="form-control" placeholder="Enter here"></textarea>
+                                            <textarea name="txtcontent" id="txtcontent" rows="8" class="form-control" placeholder="Enter here"></textarea>
                                         </div>
                                     </div>
                                     <div class="col-lg-12">
                                         <div class="form-group">
                                             <label>Upload Image:</label>
-                                            <input type="file" name="txtimage" class="form-control">
+                                            <input type="file" name="txtimage" id="txtimage" class="form-control">
                                         </div>
                                     </div>
                                     <!--<div class="col-lg-12">
@@ -75,7 +104,7 @@
                                     </div>-->
                                 </div>
                                 <div class="text-left">
-                                    <button type="submit" class="btn create-ac-btn">Submit</button>
+                                    <button type="submit" name="btnsubmit" class="btn create-ac-btn">Submit</button>
                                 </div>
                             </form>
                         </div>
--- ownblog.php
+++ ownblog.php
@@ -1,4 +1,6 @@
-
+<?php
+    include_once("DbConnection.php");
+?>
 <!DOCTYPE html>
 <html lang="zxx">
     <head>
@@ -37,25 +39,64 @@
         <section class="category-area category-area-two pt-100 pb-70">
             <div class="container">
             	<div class="row">
+                    <?php
+                        $uid=$_SESSION['UserID'];
+                        $select="SELECT * from tblblogs where Uid=$uid AND IsActive=1";
+                        $res=mysqli_query($con,$select);
+                        
+                        //Data for user name on blogs
+                        
+                        //Data for user name on blogs
+
+
+                        if($res->num_rows!=0)
+                        {
+                            while($data=$res->fetch_array()) 
+                            {
+                                /*$userid=$data['Uid'];*/
+                                $bid=$data['Bid'];
+                                $title=$data['Title'];
+                                $cont=$data['Content'];
+                                $file=$data['File'];
+                                $date=$data['CreatedDate'];
+                                $title=$data['Title'];
+
+                                if($file=="" || !file_exists("Uploaded/Images/$file"))
+                                {
+                                    $file="blog1.JFIF";
+                                }
+
+                                #For printing user name
+                                $select="SELECT * from tblregister where Uid=$uid";
+                                $Execute_sel_User=mysqli_query($con,$select) or die(mysqli_error($con));
+                                $fetch=mysqli_fetch_array($Execute_sel_User);
+                                $uname=$fetch['Fname']." ".$fetch['Lname'];
+                                #For printing user name
+                    ?>
                     <div class="col-sm-6 col-lg-4">
                         <div class="blog-item wow fadeInUp" data-wow-delay=".3s">
                             <div class="blog-top">
-                                <a href="blog-details.php">
-                                    <img src="assets/img/home-1/blog/1.jpg" alt="Blog">
+                                <a href="blog-details.php?bid=<?php echo $bid; ?>&name=<?php echo $uname; ?>">
+                                    <img src="Uploaded/Images/<?php echo $file; ?>" alt="Blog">
                                 </a>
-                                <span>21 May, 2020</span>
+                                <span><?php echo $date; ?></span>
                             </div>
                             <div class="blog-bottom">
                                 <h3>
-                                    <a href="blog-details.php">The next genaration IT will change the world</a>
+                                    <a href="blog-details.php?bid=<?php echo $bid; ?>&name=<?php echo $uname; ?>"><?php echo $title; ?></a>
                                 </h3>
                                 <ul>
                                     <li>
                                         <img src="assets/img/home-1/blog/1.png" alt="Blog">
-                                        Aikin Ward
+                                        <?php 
+                                            /*$select="SELECT * from tblregister where Uid=$uid";
+                                            $Execute_sel_User=mysqli_query($con,$select) or die(mysqli_error($con));
+                                            $fetch=mysqli_fetch_array($Execute_sel_User);*/
+                                            echo $fetch['Fname']." ".$fetch['Lname']; 
+                                        ?>
                                     </li>
                                     <li>
-                                        <a href="blog_post.php">Edit
+                                        <a href="blog_post.php?bid=<?php echo $bid; ?>">Edit
                                             <i class="icofont-simple-right"></i>
                                         </a>
 
@@ -65,18 +106,13 @@
                             </div>
                         </div>
                     </div>
+                    <?php
+                           }
+                        }
+                    ?>
                 </div>
                 
             </div>
         <!-- End Popular -->
-
-      <!-- Footer -->
-      <?php include_once('footer.php');?>
-        <!-- End Footer -->
-
-        <!-- Start scripts -->
-        <?php include_once('scriptsLinks.php');?>
-        <!-- End scripts -->
-
     </body>
 </html>
